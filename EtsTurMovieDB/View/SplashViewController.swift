0bd650cd7b8c3d8e624df3aab2c127dcdc695891
Zeliha Sena Solmaz / Emlak Katılım - Dijital Uygulamalar Servisi <Zeliha.Solmaz@emlakkatilim.com.tr>
2022-05-25 22:37:39 +0300

    firebase config
    splash screen
    remote config fetch data
    check internet connection

---
+++ EtsTurMovieDB/View/SplashViewController.swift
@@ -0,0 +1,61 @@
+//
+//  SplashViewController.swift
+//  EtsTurMovieDB
+//
+//  Created by Zeliha Sena Solmaz / Emlak Katılım - Dijital Uygulamalar Servisi on 25.05.2022.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    var splashTitle: String? = ""
+    
+    @IBOutlet var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+        if checkConnection() {
+            fetchRemoteConfig()
+        }else {
+            showAlert()
+        }
+    }
+    
+    func fetchRemoteConfig() {
+        remoteConfig.fetch(withExpirationDuration: 0) { [unowned self] (status, error) in
+            guard error == nil else { return }
+            print("Remote Config verisi geldi")
+            remoteConfig.activate()
+            displayNewValues()
+        }}
+    
+    func displayNewValues() {
+        let newLabelText = remoteConfig.configValue(forKey: "splashTitle").stringValue ?? "fff"
+        splashTitle = newLabelText
+        self.titleLabel.text = splashTitle
+    }
+    
+    func checkConnection() -> Bool
+    {
+        if Reachability.isConnectedToNetwork(){
+           return true
+        }else{
+           return false
+        }
+    }
+    
+    func showAlert() {
+        
+        self.titleLabel.text = "Hata! \n İnternet bağlantınızı \n kontrol ediniz"
+        
+//        let alert = UIAlertController(title: "Hata!", message: "İnternet bağlantınızı kontrol ediniz", preferredStyle: UIAlertController.Style.alert)
+//        alert.addAction(UIAlertAction(title: "Retry", style: .cancel, handler: { (_) in
+//             }))
+//        self.present(alert, animated: true, completion: nil)
+        
+       
+    }
+
+}
---
+++ EtsTurMovieDB/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  EtsTurMovieDB
+//
+//  Created by Zeliha Sena Solmaz / Emlak Katılım - Dijital Uygulamalar Servisi on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+       
+    }
+
+
+}
+
